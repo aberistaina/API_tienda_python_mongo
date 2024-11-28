529186fe4cac8b11cd48dfcabed1c9349ae69db4
Alejandro Beristain <alejandro_beristain@hotmail.com>
2024-11-28 20:41:14 -0300

    Creados controladores POST y GET

--- src/app.py
+++ src/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+from config.mongodb import mongo
 from productos.routes.productos_routes import productos
 
+load_dotenv()
+
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+mongo.init_app(app)
 
 
 app.register_blueprint(productos, url_prefix="/api/v1/productos")
--- src/productos/controllers/productos_controllers.py
+++ src/productos/controllers/productos_controllers.py
@@ -1,11 +1,71 @@
+from flask import request ,jsonify
+from config.mongodb import mongo
+
 def get_all_products():
-    return "All Products"
+    try:
+        products = []
+        for product in mongo.db.productos.find():
+            products.append({
+                "id": str(product["_id"]),  
+                "nombre": product["nombre"],
+                "marca": product["marca"],
+                "descripcion": product["descripcion"],
+                "precio": product["precio"],
+                "stock": product["stock"]
+            })
+
+        response_data = {
+            "data": products,
+            "message": "Productos encontrados de manera exitosa",
+            "code": 200
+        }
+
+        return jsonify(response_data), 200
+
+
+    except Exception as error:  
+        return jsonify({
+            "message": f"Ocurrió un error: {str(error)}",
+            "code": 500
+        }), 500
+    
 
 def get_products_id(id):
     return f"All Products {id}"
 
 def create_product():
-    return "Producto Creado"
+    try:
+        data = request.get_json()
+        nombre, marca, descripcion, precio, stock = data["nombre"], data["marca"], data["descripcion"], data["precio"], data["stock"]
+
+        if(nombre, marca, descripcion, precio, stock):
+            response = mongo.db.productos.insert_one({
+                "nombre": nombre,
+                "marca": marca,
+                "descripcion": descripcion,
+                "precio": precio,
+                "stock": stock
+            })
+            result = {
+                "id": str(response.inserted_id),
+                "nombre": nombre,
+                "marca": marca,
+                "descripcion": descripcion,
+                "precio": precio,
+                "stock": stock
+            }
+
+        return jsonify({
+            "data": result,
+            "message": f"El Producto {nombre} fue creado correctamente",
+            "code": 201
+        }), 201
+    
+    except Exception as error:  
+        return jsonify({
+            "message": f"Ocurrió un error: {str(error)}",
+            "code": 500
+        }), 500
 
 def update_product(id):
     return f"Producto con id {id} fue modificado"
